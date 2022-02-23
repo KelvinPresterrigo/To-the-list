f9254f7fc1c06417708e71ae894126a6dbef0dcb
Kelvin Presterrigo <kelvin42xd@gmail.com>
2022-02-23 18:15:40 -0300

    GetById, Update e Delete

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,11 @@ const routes = require("express").Router(); //metodo pra utilizar as rotas
 const TaskController = require("../controller/Taskcontroller") //impostação do metodo
 
 routes.get("/", TaskController.getAllTasks)
-routes.post("/create", TaskController.createTask)
+routes.post("/create", TaskController.createTask);
+routes.get("/getById/:id/:method", TaskController.getById);
+routes.post("/updateOne/:id", TaskController.updateOneTask);
+routes.get("/deleteOne/:id/", TaskController.deleteOneTask);
+
+
 
 module.exports = routes
